8bdadaf1936779d4edfd25a9c19aa0d90e67d842
Yuriy Zhvirblis <yuriyzdev@gmail.com>
2023-10-30 11:43:11 +0200

    article author

--- Components/Author.tsx
+++ Components/Author.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
-    return (
-        <h1>Author</h1>
-    )
-}
+// import { grpahCMSImageLoader } from '../util';
 
-export default Author
+const Author = ({ author }: any) => (
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+        <div className="absolute right-0 left-0 -top-14 flex justify-center items-center ">
+            <Image
+                unoptimized
+                // loader={grpahCMSImageLoader}
+                alt={ author.name }
+                height={ 100 }
+                width={ 100 }
+                className="align-middle rounded-full"
+                src={ author.photo.url }
+            />
+        </div>
+        <h3 className="text-white mt-4 mb-4 text-xl font-bold">{ author.name }</h3>
+        <p className="text-white text-ls">{ author.bio }</p>
+    </div>
+);
+
+export default Author;
